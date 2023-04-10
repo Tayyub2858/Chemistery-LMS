6d964f2ef1e3fec312fc908eadba71d2a07a70aa
Tayyub2858 <75058061+Tayyub2858@users.noreply.github.com>
2023-04-10 17:19:32 +0500

    update

--- src/LeandingPage/Contact/Contact.jsx
+++ src/LeandingPage/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ContactStyle.css';
 import Nav from '../../Modules/Nav/Nav';
 import ContactHandler from './ContactHandler';
-// import { Grid } from '@material-ui/core';
+import { Grid } from '@mui/material';
 function Contact() {
     const {fullName,
         setfullName,
@@ -28,16 +28,16 @@ function Contact() {
                 </div>
             </div>
             <div className="contact-form-container">
-                {/* <Grid container> */}
-                    {/* <Grid item xs={12} sm={12} md={6}> */}
+                <Grid container>
+                    <Grid item xs={12} sm={12} md={6}>
                         <div className="contact-form-txt">
                             <p className="contact-form-desc">
                                 Not sure which path is right for you? Question about Corporate training or consulting?
                                 Just want to talk? <span className='contact-form-b'>Drop us a line, we’re here to help.</span>
                             </p>
                         </div>
-                    {/* </Grid> */}
-                    {/* <Grid item xs={12} sm={12} md={6}> */}
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={6}>
                         <div className="contact-form-input">
                             <p className='input-title'>Full Name</p>
                             <input onChange={(e) => setfullName(e.target.value)} value={fullName} type='text' className='contact-input' />
@@ -47,8 +47,8 @@ function Contact() {
                             <textarea onChange={(e)=>setmessage(e.target.value)}  value={message} name="message" className='form-msg' cols="55" rows="15" />
                             <button onClick={contactInput} className="query-submit">SUBMIT</button>
                         </div>
-                    {/* </Grid> */}
-                {/* </Grid> */}
+                    </Grid>
+                </Grid>
             </div>
    </>
   )
